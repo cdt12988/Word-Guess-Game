e791fd7ca742c62c493de95f103cc8490dbfcea6
cdt12988 <cdt12988@gmail.com>
2018-06-29 20:56:08 -0500

    homework due

--- assets/javascript/canvas.js
+++ assets/javascript/canvas.js
@@ -1,259 +1,224 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
+	
+ctx.strokeStyle="#57585D";
 
-// function drawGallows() {
-	
-	ctx.strokeStyle="black";
-	
-  // Top horizontal
-/*
-  ctx.moveTo(100,25);
-  ctx.lineTo(125,25);
-  ctx.stroke();
-*/
+// Top horizontal bar
+
+ctx.moveTo(70, 45);
+ctx.lineTo(175-30, 45);
+ctx.stroke();
+
+// Rope from top bar
+
+ctx.moveTo(70, 45);
+ctx.lineTo(70, 65);
+ctx.stroke();
+
+// Main vertical post
+
+ctx.moveTo(175-30, 45);
+ctx.lineTo(175-30, 200);
+ctx.stroke();
+
+// Top cross-bar
+
+ctx.moveTo(175-30, 70);
+ctx.lineTo(150-30, 45);
+ctx.stroke();
+
+// Bottom horizontal bar
+
+ctx.moveTo(70-30, 200);
+ctx.lineTo(245-30, 200);
+ctx.stroke();
+
+// Bottom diagonal supports
+
+ctx.moveTo(175-30, 175);
+ctx.lineTo(150-30, 200);
+ctx.stroke();
+ctx.moveTo(175-30, 175);
+ctx.lineTo(200-30, 200);
+ctx.stroke();
+
+// Stickman:
+
+function drawStickman() {
+	drawHead();
+	drawBody();
+	drawLeftArm();
+	drawRightArm();
+	drawLeftLeg();
+	drawRightLeg();
+}
   
-/*
-	ctx.moveTo(85+30, 35);
-	ctx.lineTo(150+30, 35);
-	ctx.stroke();
-*/
-
-	ctx.moveTo(70, 45);
-	ctx.lineTo(175-30, 45);
-	ctx.stroke();
-
-  // Rope from top
-/*
-  ctx.moveTo(100,25);
-  ctx.lineTo(100,35);
-  ctx.stroke();
-*/
-
-/*
-	ctx.moveTo(85+30, 35);
-	ctx.lineTo(85+30, 45);			
-	ctx.stroke();
-*/
-
-	ctx.moveTo(70, 45);
-	ctx.lineTo(70, 65);
-	ctx.stroke();
-	
-/*
-  // Long vertical post
-  ctx.moveTo(125,25);
-  ctx.lineTo(125,75);
-  ctx.stroke();
-*/
-
-/*
-	ctx.moveTo(150+30, 35);
-	ctx.lineTo(150+30, 100);
-	ctx.stroke();
-*/
-
-	ctx.moveTo(175-30, 45);
-	ctx.lineTo(175-30, 200);
-	ctx.stroke();
-
-/*
-  // Bottom horizontal base
-  ctx.moveTo(100,75);
-  ctx.lineTo(150,75);
-  ctx.stroke();
-*/
-
-/*
-	ctx.moveTo(85+15, 100);
-	ctx.lineTo(205+15, 100);
-	ctx.stroke();
-*/
-
-	ctx.moveTo(70-30, 200);
-	ctx.lineTo(245-30, 200);
-	ctx.stroke();
-
-/*
-  // Bottom diagonal supports
-  ctx.moveTo(115,75);
-  ctx.lineTo(125,65);
-  ctx.stroke();
-  ctx.moveTo(135,75);
-  ctx.lineTo(125,65);
-  ctx.stroke();
-*/
-// }
-
-/*
-	ctx.moveTo(125+30, 100);
-	ctx.lineTo(150+30, 85);
-	ctx.stroke();
-	ctx.moveTo(175+30, 100);
-	ctx.lineTo(150+30, 85);
-	ctx.stroke();
-*/
-
-	ctx.moveTo(175-30, 175);
-	ctx.lineTo(150-30, 200);
-	ctx.stroke();
-	ctx.moveTo(175-30, 175);
-	ctx.lineTo(200-30, 200);
-	ctx.stroke();
-
-// drawGallows();
-
-
-  // Stickman:
-  
-/*
-// function drawHead() {
-  // Head
-  ctx.strokeStyle="black";
-  ctx.beginPath();
-  ctx.arc(100,40,5,0,2*Math.PI);		// X-cord: same as rope
-  ctx.stroke();							// Y-cord: same as rope + radius
-// }
-*/
-
-/*
-	ctx.beginPath();
-	ctx.arc(85+30, 52, 7, 0, 2*Math.PI);
-	ctx.stroke();
-*/
-
+function drawHead() {
+	ctx.strokeStyle="#57585D";
 	ctx.beginPath();
 	ctx.arc(70, 80, 15, 0, 2*Math.PI);
 	ctx.stroke();
-
-function drawBody() {
-  // Body
-  ctx.strokeStyle="black";
-  ctx.moveTo(100,45);
-  ctx.lineTo(100,55);
-  ctx.stroke();
+/*
+	clearBody();
+	clearLeftArm();
+	clearRightArm();
+	clearLeftLeg();
+	clearRightLeg();
+*/
 }
 
-/*
-	ctx.moveTo(115, 59);
-	ctx.lineTo(115, 70);
-	ctx.stroke();
-*/
-
+function drawBody() {
+	ctx.strokeStyle="#57585D";
 	ctx.moveTo(70, 95);
 	ctx.lineTo(70, 125);
 	ctx.stroke();
-
-function drawLeftArm() {
-  // Left Arm
-  ctx.strokeStyle="black";
-  ctx.moveTo(100,45);
-  ctx.lineTo(95, 55);
-  ctx.stroke();
+/*
+	clearLeftArm();
+	clearRightArm();
+	clearLeftLeg();
+	clearRightLeg();
+*/
 }
 
-/*
-	ctx.moveTo(115, 59);
-	ctx.lineTo(105, 70);
-	ctx.stroke();
-*/
-
+function drawLeftArm() {
+	ctx.strokeStyle="#57585D";
 	ctx.moveTo(70, 95);
 	ctx.lineTo(60, 125);
 	ctx.stroke();
+/*
+	clearRightArm();
+	clearLeftLeg();
+	clearRightLeg();
+*/
+}
 
 function drawRightArm() {
-  // Right Arm
-  ctx.strokeStyle="black";
-  ctx.moveTo(100,45);
-  ctx.lineTo(105,55);
-  ctx.stroke();
-}
-
-/*
-	ctx.moveTo(115, 59);
-	ctx.lineTo(125, 70);
-	ctx.stroke();
-*/
-
-	ctx.moveTo(70, 95);
+	ctx.strokeStyle="#57585D";
+  	ctx.moveTo(70, 95);
 	ctx.lineTo(80, 125);
 	ctx.stroke();
-
-function drawLeftLeg() {
-  // Left Leg
-  ctx.strokeStyle="black";
-  ctx.moveTo(100,55);
-  ctx.lineTo(95, 65);
-  ctx.stroke();
+/*
+	clearLeftLeg();
+	clearRightLeg();
+*/
 }
 
-/*
-	ctx.moveTo(115, 70);
-	ctx.lineTo(105, 81);
-	ctx.stroke();
-*/
-
+function drawLeftLeg() {
+	ctx.strokeStyle="#57585D";
 	ctx.moveTo(70, 125);
 	ctx.lineTo(60, 155);
 	ctx.stroke();
-
-function drawRightLeg() {
-  // Right Leg
-  ctx.strokeStyle="black";
-  ctx.moveTo(100,55);
-  ctx.lineTo(105,65);
-  ctx.stroke();
+// 	clearRightLeg();
 }
 
-/*
-	ctx.moveTo(115, 70);
-	ctx.lineTo(125, 81);
-	ctx.stroke();
-*/
-
+function drawRightLeg() {
+	ctx.strokeStyle="#57585D";
 	ctx.moveTo(70, 125);
 	ctx.lineTo(80, 155);
 	ctx.stroke();
-
-function clearCanvas() {
-//	var canvas = document.getElementById('myCanvas'),
-//    ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 function clearStickman() {
 	
-	ctx.strokeStyle="white";
+	ctx.strokeStyle="#E1E8F0";
 	
 	for(var i=0; i<11; i++) {
+		
+		// Head
+		ctx.beginPath();
+		ctx.arc(70, 80, 15, 0, 2*Math.PI);
+		ctx.stroke();
+		
+		// Body
+		ctx.moveTo(70, 95);
+		ctx.lineTo(70, 125);
+		ctx.stroke();
+		
+		// Left Arm
+		ctx.moveTo(70, 95);
+		ctx.lineTo(60, 125);
+		ctx.stroke();
+		
+		// Right Arm
+	  	ctx.moveTo(70, 95);
+		ctx.lineTo(80, 125);
+		ctx.stroke();
 	
-	  // Head
-	  ctx.beginPath();
-	  ctx.arc(100,40,5,0,2*Math.PI);		// X-cord: same as rope
-	  ctx.stroke();							// Y-cord: same as rope + radius
+		// Left Leg
+		ctx.moveTo(70, 125);
+		ctx.lineTo(60, 155);
+		ctx.stroke();
 	
-	  // Body
-	  ctx.moveTo(100,45);
-	  ctx.lineTo(100,55);
-	  ctx.stroke();
-	  // Left Arm
-	  ctx.moveTo(100,45);
-	  ctx.lineTo(95, 55);
-	  ctx.stroke();
-	
-	  // Right Arm
-	  ctx.moveTo(100,45);
-	  ctx.lineTo(105,55);
-	  ctx.stroke();
-	
-	  // Left Leg
-	  ctx.moveTo(100,55);
-	  ctx.lineTo(95, 65);
-	  ctx.stroke();
-	
-	  // Right Leg
-	  ctx.moveTo(100,55);
-	  ctx.lineTo(105,65);
-	  ctx.stroke();
+		// Right Leg
+		ctx.moveTo(70, 125);
+		ctx.lineTo(80, 155);
+		ctx.stroke();
+
+	}
+}
+
+function clearHead() {
+	ctx.strokeStyle="#E1E8F0";
+	for(var i=0; i<11; i++) {
+		
+		// Head
+		ctx.beginPath();
+		ctx.arc(70, 80, 15, 0, 2*Math.PI);
+		ctx.stroke();
+	}
+}
+
+function clearBody() {
+	ctx.strokeStyle="#E1E8F0";
+	for(var i=0; i<11; i++) {
+		
+		// Body
+		ctx.moveTo(70, 95);
+		ctx.lineTo(70, 125);
+		ctx.stroke();
+	}
+}
+
+function clearLeftArm() {
+	ctx.strokeStyle="#E1E8F0";
+	for(var i=0; i<11; i++) {
+		
+		// Left Arm
+		ctx.moveTo(70, 95);
+		ctx.lineTo(60, 125);
+		ctx.stroke();
+	}
+}
+
+function clearRightArm() {
+	ctx.strokeStyle="#E1E8F0";
+	for(var i=0; i<11; i++) {
+		
+		// Right Arm
+	  	ctx.moveTo(70, 95);
+		ctx.lineTo(80, 125);
+		ctx.stroke();
+	}
+}
+
+function clearLeftLeg() {
+	ctx.strokeStyle="#E1E8F0";
+	for(var i=0; i<11; i++) {
+		
+		// Left Leg
+		ctx.moveTo(70, 125);
+		ctx.lineTo(60, 155);
+		ctx.stroke();
+	}
+}
+
+function clearRightLeg() {
+	ctx.strokeStyle="#E1E8F0";
+	for(var i=0; i<11; i++) {
+		
+		// Right Leg
+		ctx.moveTo(70, 125);
+		ctx.lineTo(80, 155);
+		ctx.stroke();
 	}
 }
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,164 +1,267 @@
-var movieBank = ['forrest gump', 'lion king', 'legally blonde', 'shrek', 'gone with the wind', 'the silence of the lambs', 'full metal jacket', 'the wizard of oz', 'the lord of the rings', 'american pie', 'rocky', 'the breakfast club', 'toy story', 'beauty and the beast', 'star wars', 'harry potter', 'the hunger games', 'titanic', 'saving private ryan', 'top gun', 'anchorman', 'pulp fiction', 'the dark knight', 'pirates of the caribbean', 'james bond', 'mean girls', 'jurassic park', 'the matrix', 'back to the future', 'ferris buellers day off', 'the princess bride', 'the karate kid', 'jaws', 'the godfather', 'a clockwork orange', 'willy wonka and the chocolate factory', 'the sound of music', 'breakfast at tiffanys', 'snow white and the seven dwarves', 'citizen kane', 'training day', 'remember the titans', 'armegeddon', 'scream', 'avatar', 'cast away', 'iron man', 'the shining', 'nightmare on elm street', 'edward scissorhands', 'the wolf of wall street', 'mad max', 'lethal weapon', 'die hard', 'city slickers', 'tombstone', 'the avengers', 'the shawshank redemption', 'schindlers list', 'west side story', 'et the extra terrestrial', 'raiders of the lost ark', 'braveheart', 'the exorcist'];
-
-var sportsBank = ['babe ruth', 'dallas cowboys', 'new york yankees', 'los angeles lakers', 'michael jordan', 'wayne gretzky', 'troy aikman', 'dirk nowitzki', 'lebron james', 'tom brady', 'brett favre', 'shaquille oneal', 'muhammad ali', 'tim duncan', 'nolan ryan', 'ted williams', 'jackie robinson', 'chicago bulls', 'new york knicks', 'chicago cubs', 'new england patriots', 'new york giants', 'golden state warriors', 'san antonio spurs', 'lindsey vonn', 'david beckham', 'tiger woods', 'kareem abdul jabbar', 'dale earnhardt jr', 'lionel messi', 'roger federer', 'serena williams', 'cristiano ronaldo', 'michael phelps', 'kobe bryant', 'usain bolt', 'shaun white', 'magic johnson', 'larry bird', 'floyd mayweather', 'oscar de la hoya', 'san fransico giants', 'st louis cardinals', 'manchester united', 'philadelphia flyers', 'new orleans saints', 'new york rangers', 'texas rangers', 'dallas mavericks', 'chicago blackhawks', 'chicago bears', 'philadelphia eagles', 'atlanta braves', 'new york jets', 'mike modano', 'miami heat', 'boston celtics', 'toronto maple leafs', 'fc barcelona', 'philadelphia phillies', 'pittsburgh steelers', 'bosto red sox', 'green bay packers', 'real madrid', 'alexander ovechkin', 'stephen curry', 'kevin durant', 'willie mays', 'hank aaron', 'ty cobb', 'mickey mantle', 'barry sanders', 'emmitt smith', 'jerry rice', 'peyton manning', 'mike tyson', 'bo jackson', 'conor mcgregor']
-
+// Set game variables and arrays
 var wins = 0;
 var losses = 0;
 var streak = 0;
+var game = false;
+
+var spaces = 0;
+var numLetters = 0;
+var strikesLeft = 6;
+var remainingLetters = 0;
+var message = '';
+var catText = '';
+var verb = '';
+var prevGuesses = []
+var wordLength = [];
+var word = [];
+
+// Set DOM capture variables
+var letters = document.getElementsByClassName('letters');
+var category = document.getElementById('category');
+
+var messDisplay = document.getElementById('messDisplay');
+var wordDisplay = document.getElementById('wordDisplay');
+var catDisplay = document.getElementById('catDisplay');	
+var winsDisplay = document.getElementById('winsDisplay');
+var lossesDisplay = document.getElementById('lossesDisplay');
+var streakDisplay = document.getElementById('streakDisplay');
+
+// Set keyboard event listeners
+document.onkeyup = function(event) {
+	if(game) {
+		letterGuessed(event.key);
+	} else {
+		play();
+	}
+}
+
+// Set buttons event listeners	
+for(var i =0; i < letters.length; i++) {
+	letters[i].addEventListener('click', function(event) {
+		var target = event.target;
+		var targetId = target.id;
+		if(game) {
+			letterGuessed(targetId);
+		} else {
+			play();
+		}
+	});
+}
+
+function displayMessage(msg) {
+	messDisplay.textContent = msg;
+}
+
+function resetButtons() {
+	for (var i = 0; i < letters.length; i++) {
+		letters[i].className = 'letters';
+	}
+}
 
 function play() {
+
+	// Checks if a current game exists already
+	if(!game) {
 	
-	var category = document.getElementById('category');
-	var catText = '';
-	
-	if(category.selectedIndex == 0) {
-		wordBank = movieBank;
-		catText = 'Movies';
-	} else {
-		wordBank = sportsBank;
-		catText = 'Sports';
-	}
-	
-	var word = wordBank[Math.floor(Math.random() * wordBank.length)];
-	
-	var wordLength = [];
-	
-	var spaces = 0;
-	var numLetters = 0;
-	var verb = '';
-	var game = true;
-	
-	var wordDisplay = document.getElementById('wordDisplay');
-	var catDisplay = document.getElementById('catDisplay');
-	var messDisplay = document.getElementById('messDisplay');
-	var guessDisplay = document.getElementById('guessDisplay');
-	var winsDisplay = document.getElementById('winsDisplay');
-	var lossesDisplay = document.getElementById('lossesDisplay');
-	var streakDisplay = document.getElementById('streakDisplay');
-	
-	function displayMessage(msg) {
-		messDisplay.textContent = msg;
-	}
-	
-	var message = '';
-	
-	for(var i = 0; i < word.length; i++) {
-		if (word[i] === ' ') {
-			spaces++;
-			wordLength[i] = '\x20\x20';
+	// Resets initial game variables	
+		game = true;
+		wordLength = [];
+		prevGuesses = [];
+		verb = '';
+		spaces = 0;
+		numLetters = 0;
+		strikesLeft = 6;
+		
+	// Assigns the category to the selected category
+		if(category.selectedIndex == 0) {
+			var randomIndex = Math.floor(Math.random()*randomBank.length);		
+			wordBank = randomBank[randomIndex];
+			catText = randomCat[randomIndex];
+		} else if(category.selectedIndex == 1) {
+			wordBank = sportsBank;
+			catText = 'Sports';
+		} else if(category.selectedIndex == 2) {
+			wordBank = countriesBank;
+			catText = 'Countries';
+		} else if(category.selectedIndex == 3) {
+			wordBank = statesBank;
+			catText = 'States';
+		} else if(category.selectedIndex == 4) {
+			wordBank = presidentsBank;
+			catText = 'US Presidents';
+		} else if(category.selectedIndex == 5) {
+			wordBank = animalBank;
+			catText = 'Animals';
+		} else if(category.selectedIndex == 6) {
+			wordBank = musicBank;
+			catText = 'Music';
+		} else if(category.selectedIndex == 7) {
+			wordBank = movieBank;
+			catText = 'Movies';
 		} else {
-			wordLength[i] = '_';
+			wordBank = comicsBank;
+			catText = 'Comic Books';
+		}
+		
+	// Assigns the word to a random word within the category
+		word = wordBank[Math.floor(Math.random() * wordBank.length)];
+		
+	// Assings the individual letters of the word into an array of underscores and spaces
+		for(var i = 0; i < word.length; i++) {
+			if (word[i] === ' ') {
+				spaces++;
+				wordLength[i] = '\x20\x20';
+			} else {
+				wordLength[i] = '_';
+			}
+		}
+		
+	// Assigns the win-condition (letters left to be guessed) to the length of the word minus any spaces
+		remainingLetters = word.length - spaces;
+		
+	/*
+		console.log('word: ' + word);
+		console.log('word length: ' + wordLength);
+		console.log('spaces: ' + spaces);
+		console.log('remaining letters: ' + remainingLetters);
+	*/
+	
+	// Displays the current word (in underscores) and category in the game	
+		wordDisplay.textContent = wordLength.join('\xa0');
+		catDisplay.textContent = catText;
+	
+	// Resets the visual stylings for the stickman drawing and buttons, and changes the initial game message	
+		drawStickman();
+		clearStickman();
+		resetButtons();
+		displayMessage('Let\'s get started! Guess a letter!');
+	
+	// Confirms starting a new game if a current one exists -- if yes, then performs game loss actions
+	} else {
+		var restart = confirm('Starting a new game will cause you to lose your current one.  Would you like to start a new one anyway?');
+		if(restart) {
+			losses++;
+			streak = 0;
+			game = false;
+			winsDisplay.textContent = wins;
+			lossesDisplay.textContent = losses;
+			streakDisplay.textContent = streak;
+			play();
 		}
 	}
+}
+
+function letterGuessed(userKey) {
 	
-	var remainingLetters = word.length - spaces;
-	var strikesLeft = 6;
-	var prevGuesses = []
-	
-	console.log('word: ' + word);
-	console.log('word length: ' + wordLength);
-	console.log('spaces: ' + spaces);
-	console.log('remaining letters: ' + remainingLetters);
-	
-	wordDisplay.textContent = wordLength.join('\xa0');
-	catDisplay.textContent = catText;
-	
-	clearStickman();
-	displayMessage('Let\'s get started! Guess a letter!');
-	
-	document.onkeyup = function(event) {
-		var userKey = event.key;
-		console.log('user key: ' + userKey);
+	// Tests if user input is a letter or not		
+	if (/^[a-z]+$/.test(userKey)) {
+		
+	// Tests if letter has been guessed previously -- if not, adds it to the previous guesses array, and changes the button display styling
+		if(prevGuesses.indexOf(userKey) < 0) {
+			prevGuesses.push(userKey);
+			console.log('guesses: ' + prevGuesses);
+			document.getElementById(userKey).classList.add('guessed');
 			
-		if (/^[a-z]+$/.test(userKey)) {
-			
-			if(prevGuesses.indexOf(userKey) < 0) {
-				prevGuesses.push(userKey);
-				console.log('guesses: ' + prevGuesses);
-				guessDisplay.textContent = prevGuesses.join(' ').toUpperCase();
+	// Tests if the guessed letter is in the word
+			if(word.indexOf(userKey) >= 0) {
 				
-				if(word.indexOf(userKey) >= 0) {
-// 					alert('You guessed a letter!');
-					
-					for(var i = 0; i < word.length; i++) {
-						if(userKey === word[i]) {
-							wordLength[i] = userKey.toUpperCase();
-							remainingLetters--;
-							numLetters++;
-							wordDisplay.textContent = wordLength.join('\xa0');
-							console.log('remaining letters: ' + remainingLetters);
-						}
-					}
-					if(numLetters > 1) {
-						var verb = 'were';
-						var plural = '\'s'
-					} else {
-						var verb = 'was';
-						var plural = '';
-					}
-					displayMessage('Nice! There ' + verb + ' ' + numLetters + ' ' + userKey.toUpperCase() + plural + '!');
-					numLetters = 0;
-					
-					if(remainingLetters <= 0) {
-						wins++;
-						streak++;
-// 						alert('YOU WIN!');
-						message = 'Congrats! You Win!';
-						if(strikesLeft == 6) {
-							displayMessage('WOW! A Flawless Win! Well done!');
-						} else {
-							displayMessage('Congrats! You Win!');
-						}
-						console.log('message: ' + message);
-						console.log(wins + losses + streak);
-						winsDisplay.textContent = wins;
-						lossesDisplay.textContent = losses;
-						streakDisplay.textContent = streak;
-						game = false;
-						// Perform Game-Winning tasks and break out of function?
-					}
-				} else {
-// 					alert('Try again!');
-					displayMessage('Oh no! There weren\'t any ' + userKey.toUpperCase() + '\'s!');
-					if(strikesLeft == 6) {
-						drawHead();
-					} else if(strikesLeft == 5) {
-						drawBody();
-					} else if(strikesLeft == 4) {
-						drawLeftArm();
-					} else if(strikesLeft == 3) {
-						drawRightArm();
-					} else if(strikesLeft == 2) {
-						drawLeftLeg();
-					} else {
-						drawRightLeg();
-					}
-					strikesLeft--;
-					if(strikesLeft <= 0) {
-						losses++;
-						streak = 0;
-// 						alert('YOU LOSE! Better luck next time!');
-						message = 'Oh no! You\'ve lost! Better luck next time!';
-						displayMessage('Oh no! You\'ve lost! Better luck next time!');
-						console.log('message: ' + message);
-						winsDisplay.textContent = wins;
-						lossesDisplay.textContent = losses;
-						streakDisplay.textContent = streak;
-						game = false;
-						// Fill in the rest of the letters, game-losing tasks and break out of function?
+	// Runs through the word array, replaces relavent underscores with the letter guessed, adjusts variables accordingly, and changes the word display in game
+				for(var i = 0; i < word.length; i++) {
+					if(userKey === word[i]) {
+						wordLength[i] = userKey.toUpperCase();
+						remainingLetters--;
+						numLetters++;
+						wordDisplay.textContent = wordLength.join('\xa0');
+						console.log('remaining letters: ' + remainingLetters);
 					}
 				}
+	
+	// Sets grammatical syntax to singular or plural for message displaying purposes
+				if(numLetters > 1) {
+					var verb = 'were';
+					var plural = '\'s'
+				} else {
+					var verb = 'was';
+					var plural = '';
+				}
+				
+	// Displays message with the number of correctly guessed letters; then resets the counter variable to be used again later
+				displayMessage('Nice! There ' + verb + ' ' + numLetters + ' ' + userKey.toUpperCase() + plural + '!');
+				numLetters = 0;
+	
+	// Checks win condition and adjusts variables accordingly			
+				if(remainingLetters <= 0) {
+					wins++;
+					streak++;
+					message = 'Congrats! You Win!';
+	// Displays a separate game winning message if there were no incorrect guesses during the game
+					if(strikesLeft == 6) {
+						displayMessage('WOW! A Flawless Win! Well done!');
+					} else if(strikesLeft == 1) {
+						displayMessage('Whoah, that was a close one! Well done! You win!');
+					} else {
+						displayMessage('Congrats! You Win!');
+					}
+	// Updates the scoreboard display and ends the game
+					console.log('message: ' + message);
+					console.log(wins + losses + streak);
+					winsDisplay.textContent = wins;
+					lossesDisplay.textContent = losses;
+					streakDisplay.textContent = streak;
+					game = false;
+				}
+				
+	// If guessed letter is not in the word, change the game message accordingly
 			} else {
-				message = 'You\'ve already guessed ' + userKey.toUpperCase() + '!';
-				console.log('message: ' + message);
-				displayMessage('You\'ve already guessed ' + userKey.toUpperCase() + '!');
+				displayMessage('Oh no! There weren\'t any ' + userKey.toUpperCase() + '\'s!');
+				
+	// Draws the appropriate portion of the stick figure to represent the number of guesses left and decrements the lose condition
+				if(strikesLeft == 6) {
+					drawHead();
+				} else if(strikesLeft == 5) {
+					drawBody();
+				} else if(strikesLeft == 4) {
+					drawLeftArm();
+				} else if(strikesLeft == 3) {
+					drawRightArm();
+				} else if(strikesLeft == 2) {
+					drawLeftLeg();
+				} else {
+					drawRightLeg();
+				}
+				strikesLeft--;
+				
+	// After decrimenting the lose condition, tests the lose condition and adjusts variables and messages accordingly, and ends the game
+				if(strikesLeft <= 0) {
+					losses++;
+					streak = 0;
+					message = 'Oh no! You\'ve lost! Better luck next time!';
+					displayMessage('Oh no! You\'ve lost! Better luck next time!');
+					console.log('message: ' + message);
+					winsDisplay.textContent = wins;
+					lossesDisplay.textContent = losses;
+					streakDisplay.textContent = streak;
+					game = false;
+	// Fills in and displays the remaining letters of the word
+					for(var i = 0; i < wordLength.length; i++) {
+						wordLength[i] = word[i];
+						wordDisplay.textContent = wordLength.join('\xa0').toUpperCase();
+					}
+				}
 			}
 			
+	// If the guessed letter has already been guessed, update the message accordingly
 		} else {
-			message = 'That\'s not a letter! Please type a letter!';
+			message = 'You\'ve already guessed ' + userKey.toUpperCase() + '!';
 			console.log('message: ' + message);
-			displayMessage('That\'s not a letter! Please type a letter!');
+			displayMessage('You\'ve already guessed ' + userKey.toUpperCase() + '!');
 		}
-	}
 	
+	// If the user input was not a letter, updates message accordingly	
+	} else {
+		message = 'That\'s not a letter! Please type a letter!';
+		console.log('message: ' + message);
+		displayMessage('That\'s not a letter! Please type a letter!');
+	}
+	console.log('strikes: ' + strikesLeft);
 }
